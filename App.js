7d5d4148ecaf76edebedf2cba3ecbf9bf32b95d5
KatjaMN <katjamarianyman@gmail.com>
2022-04-01 16:06:46 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput } from 'react-native';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [number] = useState(Math.floor(Math.random() * 100) + 1);
+  
+  
+    const [count, setCount] = useState(1);
+
+  //Counting the amount of guesses made.
+  
+
+  //Comparing the guessed number to the random number, that was set when starting the app.
+  const guess = () => {
+    setCount(count + 1);
+    if (Number(text) < number) {
+      setAnswer(`${text} is too low.`)
+    } else if (Number(text) > number) {
+      setAnswer(`${text} is too high.`)
+    } else {
+      setAnswer(`${text} is right!`)
+      
+      Alert.alert(`You guessed the number in ${ count } guesses`);
+    }
+  }
+
+  //{() => {setCount(count + 1)}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Guess a number between 1-100: </Text>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+      <View style={styles.row}>
+        <View style={styles.button}>
+          <Button onPress={ guess } title='Make guess' />
+        </View>
+      </View>
+      <Text style={{ marginTop: 20 }}>Your guess {answer}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +53,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    width: '20%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 5,
+    paddingLeft: 5
+  },
+
+  row: {
+    flexDirection: "row",
+    justifyContent: 'space-evenly',
+    width: '50%',
+  },
+
+  button: {
+    marginTop: 10,
+  }
+
 });
+
